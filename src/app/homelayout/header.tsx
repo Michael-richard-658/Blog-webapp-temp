6f3427d175fe9ecdca1f43d275c93ec236a5ebea
Michael Richard <michaelrichard7020@gmail.com>
2025-02-16 20:23:41 +0530

    just changing colors

---
+++ src/app/homelayout/header.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div className='header'>
+        <h1 className='header-title'><Link href={'/'} className='header-l'>Header</Link></h1>
+        <div className='nav-cont'>
+            <a href="/blogs" className='nav-1'>Blogs</a>
+            <a href="/about" className='nav-2'>About</a>
+            <a href="/contact" className='nav-3'>Contact</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
